9607253d0c777901984534ecd4f7bfff8722260a
Your Name <estevao.bueno@estudante.ifms.edu.br>
2025-08-27 10:41:15 -0400

    Inicialização do Código

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
+import express from 'express'
+import mysql from 'mysql2/promise'
 import 'dotenv/config'
+
+const app = express()
+app.get('/', (req, res) =>{
+    res.send(process.env.DBHOST)
+})
 console.log(process.env.DBHOST);
