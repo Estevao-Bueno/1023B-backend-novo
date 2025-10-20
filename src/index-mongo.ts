91157da76e9f06f23d887a5dd4a5bc427ca14af8
Estevao-Bueno <estevao.bueno@estudante.ifms.edu.br>
2025-10-20 07:51:50 -0400

    app.use para rotas não autenticadas

--- src/index-mongo.ts
+++ src/index-mongo.ts
@@ -3,14 +3,16 @@ import 'dotenv/config'
 import rotasNaoAutenticadas from './rotas/rotas-nao-autenticadas.js'
 import rotasAutenticadas from './rotas/rotas-autenticadas.js'
 import Auth from './middlewares/auth.js'
+import cors from 'cors'
 const app = express()
+app.use(cors())
 
 app.use(express.json())
 
 
 // Usando as rotas definidas em rotas.ts
-app.use(Auth,rotasAutenticadas)
 app.use(rotasNaoAutenticadas)
+app.use(Auth,rotasAutenticadas)
 
 // Criando o servidor na porta 8000 com o express
 app.listen(8000, () => {
